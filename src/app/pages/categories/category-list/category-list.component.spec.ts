2eb6bfe75c98294415d1bd2b26cfb1b7a132020f
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2020-11-20 22:46:12 -0300

    feat(category): improving list component template

--- src/app/pages/categories/category-list/category-list.component.spec.ts
+++ src/app/pages/categories/category-list/category-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { CategoryListComponent } from './category-list.component';
 
 describe('CategoryListComponent', () => {
@@ -8,9 +8,9 @@ describe('CategoryListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CategoryListComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [CategoryListComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +21,57 @@ describe('CategoryListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have a breadcrumb-item link 'Home'`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.breadcrumb-item a').textContent).toContain(
+      'Home'
+    );
+  });
+
+  it(`should have a breadcrumb-item 'Categorias'`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(
+      compiled.querySelector('.breadcrumb-item:nth-child(2)').textContent
+    ).toContain('Categorias');
+  });
+
+  it(`should render category title as 'Categorias'`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('div > div > h1.h2').textContent).toContain(
+      'Categorias'
+    );
+  });
+
+  it(`should have a table head column 'Categoria'`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('th:nth-child(1)').textContent).toContain(
+      'Categoria'
+    );
+  });
+
+  it(`should have a table head column 'Ações'`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('th:nth-child(2)').textContent).toContain(
+      'Ações'
+    );
+  });
+
+  it(`should have a table with tbody`, () => {
+    fixture = TestBed.createComponent(CategoryListComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('table tbody')).toBeTruthy();
   });
 });
